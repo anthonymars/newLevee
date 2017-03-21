b6654142a3db20fe93b0bd49d8f309f9570709ba
anthonymars <anthony.marstiller@gmail.com>
2017-03-21 14:05:23 -0400

    fb stuff

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,10 +12,19 @@
       </div>
     </div>
     <div class="col-md-3">
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1692161187688927";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
       <div class="socialSpacing">
         <a class="twitter-timeline" data-width="250" data-height="400" href="https://twitter.com/liveonleveewv">Tweets by liveonleveewv</a>
         <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
     </div>
   </div>
+  <div id="fb-root"></div>
 </footer>
