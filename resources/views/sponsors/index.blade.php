177ad7c24182939c0af6fffc74d8987c24b2d13a
anthonymars <anthony.marstiller@gmail.com>
2017-03-22 13:33:57 -0400

    sponsor fix

--- resources/views/sponsors/index.blade.php
+++ resources/views/sponsors/index.blade.php
@@ -22,25 +22,31 @@
       </div>
       @else
       @foreach ($sponsors as $s)
+      @if ($s->sponsor_type === "a_title")
       <h3 class="text-center">Title Sponsor</h3>
       <hr>
-      @if ($s->sponsor_type === "a_title")
         <img src="/images/sponsors/logos/{{$s->sponsor_logo}}" alt="title sponsor" class="img img-responsive">
         <h4>{{$s->sponsor_name}}</h4>
         <p>{{$s->sponsor_desc}}</p>
         <p><a href="http://www.{{$s->sponsor_website}}">{{$s->sponsor_website}}</a></p>
-      @endif
       <hr>
 
       <h3 class="text-center">Diamond</h3>
       <hr>
+      @else
       @if ($s->sponsor_type === "b_diamond")
         <img src="/images/sponsors/logos/{{$s->sponsor_logo}}" alt="title sponsor" class="img img-responsive">
         <h4>{{$s->sponsor_name}}</h4>
         <p>{{$s->sponsor_desc}}</p>
         <p><a href="http://www.{{$s->sponsor_website}}">{{$s->sponsor_website}}</a></p>
+        <hr>
       @endif
-      <hr>
+
+      @endif
+
+
+
+
 
       <h3 class="text-center">Platinum</h3>
       <hr>
