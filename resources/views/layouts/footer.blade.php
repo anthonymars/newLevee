4154e354695d9f49e3138d8a4959ff4fd2d68897
anthonymars <anthony.marstiller@gmail.com>
2017-03-28 11:47:27 -0400

    footer fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,31 +1,44 @@
 <footer>
-  <div class="row">
-    <div class="col-md-3"></div>
-    <div class="col-md-3"></div>
-    <div class="col-md-3">
-      <div class="socialSpacing">
-        <div class="fb-page" data-href="https://www.facebook.com/liveonthelevee/"
-          data-tabs="timeline" data-width="250" data-height="400" data-small-header="false"
-          data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-          <blockquote cite="https://www.facebook.com/liveonthelevee/" class="fb-xfbml-parse-ignore">
-            <a href="https://www.facebook.com/liveonthelevee/">Live on the Levee</a></blockquote>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="../images/pages/logos/loLarger.png" alt="LOL" class="img img-responsive">
+      </div>
+
+      <div class="col-md-3">
+        <h4>Facebook</h4>
+        <hr>
+        <div class="socialSpacing">
+          <div class="fb-page" data-href="https://www.facebook.com/liveonthelevee/"
+            data-tabs="timeline" data-width="250" data-height="400" data-small-header="false"
+            data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/liveonthelevee/" class="fb-xfbml-parse-ignore">
+              <a href="https://www.facebook.com/liveonthelevee/">Live on the Levee</a></blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <h4>Twitter</h4>
+        <hr>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1692161187688927";
+          fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <div class="socialSpacing">
+          <a class="twitter-timeline" data-width="250" data-height="400" href="https://twitter.com/liveonleveewv">Tweets by liveonleveewv</a>
+          <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
       </div>
     </div>
-    <div class="col-md-3">
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1692161187688927";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-      </script>
-      <div class="socialSpacing">
-        <a class="twitter-timeline" data-width="250" data-height="400" href="https://twitter.com/liveonleveewv">Tweets by liveonleveewv</a>
-        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-      </div>
-    </div>
+    <hr>
+    <p>©2017 City of Charleston. All Rights Reserved.</p>
   </div>
+
+
+
   <div id="fb-root"></div>
 </footer>
